d1a6ed44ca7fdaaac0abf37137fcf53a2f5af293
Chris Brocklesby <Chris@Energist-Macbook.local>
2017-04-11 15:17:42 +0100

    V1.0

--- app.rb
+++ app.rb
@@ -28,25 +28,25 @@ error 500 do
 end
 
 ###### Home Route #######
-get "/" do
+get "/?" do
   @pageTitle = "Home Page"
   erb :home
 end
 
 ###### Post Index Route #######
-get "/post" do
+get "/post/?" do
   @pageTitle = "Post Index"
   @posts = Post.all # Get all posts.
   erb :"post/index"
 end
 
 ###### New Post Routes #######
-get "/post/new" do
+get "/post/new/?" do
   @pageTitle = "New Post"
   erb :"post/new"
 end
 
-post "/post/new" do
+post "/post/new/?" do
   data = Post.new
   data.title = params[:title]
   data.slug = params[:slug]
@@ -60,13 +60,13 @@ post "/post/new" do
 end
 
 ###### Edit Post Routes #######
-get "/post/edit/:id" do
+get "/post/edit/:id/?" do
   @pageTitle = "Edit Post"
   @post = Post.get(params[:id]) # Get post by ID.
   erb :"post/edit"
 end
 
-post "/post/edit/:id" do
+post "/post/edit/:id/?" do
   data = Post.get(params[:id]).update(
     :title => params[:title], 
     :slug => params[:slug], 
@@ -81,14 +81,14 @@ post "/post/edit/:id" do
 end
 
 ###### View Post Route #######
-get "/post/:id" do
+get "/post/:id/?" do
   @pageTitle = "View Post"
   @post = Post.get(params[:id]) # Get post by ID.
   erb :"post/view"
 end
 
 ###### Delete Post Route #######
-get "/post/delete/:id" do
+get "/post/delete/:id/?" do
   Post.get(params[:id]).destroy # Get post by ID then delete.
   redirect('/post')
 end
